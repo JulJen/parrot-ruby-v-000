6f680f959c386aa8f55a8e7f100564fccdff2627
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-13 18:03:07 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 
-def parrot(phrase = "Squawk!")
+def parrot(phrase)
   "Squawk!"
+  phrase = "Squawk!"
   puts phrase
   # phrase = "Squawk!"
   # puts phrase
